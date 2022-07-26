200b205039569d0cddc10ef49abfca4dc8c3a399
Gonendo <shikigami@gmail.com>
2022-07-26 20:11:07 +0200

    Fix project loading on Linux

---
+++ publish.bat
@@ -0,0 +1,1 @@
+dotnet publish chipeur.csproj -c Release -r win-x64 -p:Os=Windows -p:PublishSingleFile=true -p:DebugType=None --self-contained false
--- publish.sh
+++ publish.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-if [ ${#@} -eq 0 ]; then
-    echo "Missing platform argument: linux|windows"
-else
-    if [ $1 = "linux" ]; then
-        dotnet publish chipeur.csproj -c Release -r linux-x64 -p:Os=Linux -p:PublishSingleFile=true -p:DebugType=None --self-contained false
-    elif [ $1 = "windows" ]; then
-        dotnet publish chipeur.csproj -c Release -r win-x64 -p:Os=Windows -p:PublishSingleFile=true -p:DebugType=None --self-contained false
-    fi
-    find . -type f -path '*publish*/*' -name "*.dll.config" -delete
-fi
+dotnet publish chipeur.csproj -c Release -r linux-x64 -p:Os=Unix -p:PublishSingleFile=true -p:DebugType=None --self-contained false
